56d22eb9298b0d324de54e0cadf91ce7aacc0c00
wolfez <wolfderechter@gmail.com>
2023-12-12 21:21:07 +0100

    update base url

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://wolfderechter.github.io',
-  base: '/wolfez-portfolio',
+  site: 'https://wolfderechter.github.io/wolfez-portfolio',
+  base: '/',
 });
