b57cf2c6d7c11b4845f7649eed9f211959d1c41a
wolfez <wolfderechter@gmail.com>
2023-12-12 21:25:01 +0100

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,5 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   integrations: [tailwind()],
   site: 'https://wolfderechter.github.io',
-  base: '/wolfez-portfolio',
+  base: '/',
 });
