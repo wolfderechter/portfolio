37c16a4d75c76db68f7ccadf2014b4d99c5bd2e9
wolfez <wolfderechter@gmail.com>
2025-05-21 21:40:53 +0200

    bump deps

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 import { defineConfig } from 'astro/config';
 import cloudflare from '@astrojs/cloudflare';
-import tailwind from "@astrojs/tailwind";
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [],
   output: 'server',
-  adapter: cloudflare()
+  adapter: cloudflare(),
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
